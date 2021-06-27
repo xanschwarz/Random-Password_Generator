8440282dbec1dc07011fa367da911d873ec7f1df
Xander <xanschwarz@gmail.com>
2021-06-26 17:00:41 -0700

    Added functions to return random characters.

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Assignment Code.
-// Assigns variable to the generate button.
+// Assignment Code. Assigns variable to the generate button.
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input.
-// Function to execute when the generate button is pressed.
+// Write password to the #password input. Function to execute when the generate button is pressed.
 function writePassword() {
-  // Assigns variable to the result of calling an unwritten function called generatePassword.
-  var password = generatePassword();
-  // Assigns variable to the text area element.
-  var passwordText = document.querySelector("#password");
-
-  // Changes the text area element value to what is stored in variable password.
-  passwordText.value = password;
+  
+  
+  // The result of the generatePassword function is stored as variable password. Then the text area element is assigned to
+  // passwordText, and the value of that is set equal to variable password.
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  // passwordText.value = password;
 
 }
 
@@ -19,9 +17,21 @@ function writePassword() {
 // When the generate button is clicked, call writePassword function.
 generateBtn.addEventListener("click", writePassword);
 
-
-
-// writePassword executes on button click, so it starts everything.
-// That should queue prompts, save responses, then call generatePassword.
-// I *think* that will get everything done, with additions like styling
-// by my preference.
+// The following functions serve to return a random lower case, upper case, number, or special character, respectively.
+function randomLowerLetter() {
+  var lowers = 'abcdefghijklmnopqrstuvwxyz';
+  return lowers[Math.floor(Math.random() * lowers.length)];
+}
+function randomUpperLetter() {
+  var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return uppers[Math.floor(Math.random() * uppers.length)];
+}
+function randomNumber() {
+  var numbers = '0123456789';
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function randomSpecialCharacter() {
+  var specials = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', 
+  '/', ':', ';', '<', '=', '>','?', '@', '[', '\\', ']',  '^', '_', '`', '{', '|', '}', '~'];
+  return specials[Math.floor(Math.random() * specials.length)];
+}
