6ee14abe114351848b9da4365891dd99e9b9d614
Xander <xanschwarz@gmail.com>
2021-06-26 17:04:35 -0700

    Request user input for password criteria. Check for valid input and request again as appropriate.

--- script.js
+++ script.js
@@ -3,8 +3,40 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input. Function to execute when the generate button is pressed.
 function writePassword() {
-  
-  
+  // Prompt for password length, check for correct input. If length entered is within the range 8 to 128 and is a
+  // whole, numeric value it is stored. Otherwise user is prompted to re-enter.
+  var checkLength;
+  var passwordLength;
+  while (!checkLength) {
+    if ((passwordLength % 1 === 0) && (passwordLength >= 8) && (passwordLength <= 128)) {
+      passwordLength = +passwordLength;
+      checkLength = true;
+    }
+    else {
+      passwordLength = prompt('Please enter a password length, as a whole number, between 8 and 128.');
+    }
+  }
+  // Other password criteria, with a similar input check as above.
+  var checkCriteria;
+  var passwordLower = confirm('Should the password contain lower case letters?');
+  var passwordUpper = confirm('Should the password contain upper case letters?');
+  var passwordNumber = confirm('Should the password contain numbers?');
+  var passwordSpecial = confirm('Should the password contain special characters?');
+  while (!checkCriteria) {
+    if ((passwordLower == true) || (passwordUpper == true) || (passwordNumber == true) || (passwordSpecial == true)) {
+      checkCriteria = true;
+    }
+    else {
+      alert('You must select at least one character type to generate a password.');
+      passwordLower = confirm('Should the password contain lower case letters?');
+      passwordUpper = confirm('Should the password contain upper case letters?');
+      passwordNumber = confirm('Should the password contain numbers?');
+      passwordSpecial = confirm('Should the password contain special characters?');
+    }
+  }
+
+
+
   // The result of the generatePassword function is stored as variable password. Then the text area element is assigned to
   // passwordText, and the value of that is set equal to variable password.
   // var password = generatePassword();
