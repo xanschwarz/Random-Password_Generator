8cc1e15a8473f17aee4fa9449d9bd931752309c2
Xander <xanschwarz@gmail.com>
2021-06-26 18:39:51 -0700

    Moved generatePassword outside of writePassword. Reordered functions for readability. Edited and added comments for better descriptions of what the script is doing.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Assignment Code. Assigns variable to the generate button.
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input. Function to execute when the generate button is pressed.
+// Function writePassword asks for user input for the password criteria, checking for minimum and valid criteria. It passes the
+// criteria into function generatePassword and displays the result on screen. Styling change to indicate completion.
 function writePassword() {
   // Prompt for password length, check for correct input. If length entered is within the range 8 to 128 and is a
   // whole, numeric value it is stored. Otherwise user is prompted to re-enter.
@@ -35,57 +36,61 @@ function writePassword() {
     }
   }
 
-  function generatePassword(length, lower, upper, number, special) {
-    var genPassword = '';
-    var passwordCriteria = [];
-    if (lower == true) {
-      genPassword += randomLowerLetter();
-      passwordCriteria.push('lower');
-    }
-    if (upper == true) {
-      genPassword += randomUpperLetter();
-      passwordCriteria.push('upper');
-    }
-    if (number == true) {
-      genPassword += randomNumber();
-      passwordCriteria.push('number');
-    }
-    if (special == true) {
-      genPassword += randomSpecialCharacter();
-      passwordCriteria.push('special');
-    }
-    var remainingLength = length - genPassword.length;
-    for (i = 0; i < remainingLength; i++) {
-      var whichRandomChar = passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
-      if (whichRandomChar == 'lower') {
-        genPassword += randomLowerLetter();
-      }
-      else if (whichRandomChar == 'upper') {
-        genPassword += randomUpperLetter();
-      }
-      else if (whichRandomChar == 'number') {
-        genPassword += randomNumber();
-      }
-      else if (whichRandomChar == 'special') {
-        genPassword += randomSpecialCharacter();
-      }
-    }
-    return genPassword;
-  }
-
-
   // The result of the generatePassword function is stored as variable password. Then the text area element is assigned to
   // passwordText, and the value of that is set equal to variable password.
   var password = generatePassword(passwordLength, passwordLower, passwordUpper, passwordNumber, passwordSpecial);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   passwordText.setAttribute("style", "border-style:solid; border-color:black");
-
 }
 
-// Add event listener to generate button.
-// When the generate button is clicked, call writePassword function.
-generateBtn.addEventListener("click", writePassword);
+// Fucntion generatePassword takes the users password criteria
+function generatePassword(length, lower, upper, number, special) {
+  // Setting the local password to an empty string and criteria to an empty array.
+  var genPassword = '';
+  var passwordCriteria = [];
+  // If a given criteria was selected one random character of that type is added to the password string and that criteria
+  // type is added to the local criteria array.
+  if (lower == true) {
+    genPassword += randomLowerLetter();
+    passwordCriteria.push('lower');
+  }
+  if (upper == true) {
+    genPassword += randomUpperLetter();
+    passwordCriteria.push('upper');
+  }
+  if (number == true) {
+    genPassword += randomNumber();
+    passwordCriteria.push('number');
+  }
+  if (special == true) {
+    genPassword += randomSpecialCharacter();
+    passwordCriteria.push('special');
+  }
+  // The remaining number of characters necessary to fulfill the users criteria is determined.
+  var remainingLength = length - genPassword.length;
+  // Loop through for the number of remaining characters. Each loop generates a random integer ranging from 1 to the number of
+  // criteria selected by the user. This is used to index the passwordCriteria array and assign that value to whichRandomChar,
+  // which is then checked for which possible criteria it matches. This match calls the corresponding random character function
+  // and adds it to the genPassword string.
+  for (i = 0; i < remainingLength; i++) {
+    var whichRandomChar = passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
+    if (whichRandomChar == 'lower') {
+      genPassword += randomLowerLetter();
+    }
+    else if (whichRandomChar == 'upper') {
+      genPassword += randomUpperLetter();
+    }
+    else if (whichRandomChar == 'number') {
+      genPassword += randomNumber();
+    }
+    else if (whichRandomChar == 'special') {
+      genPassword += randomSpecialCharacter();
+    }
+  }
+  // Return the completed password.
+  return genPassword;
+}
 
 // The following functions serve to return a random lower case, upper case, number, or special character, respectively.
 function randomLowerLetter() {
@@ -105,3 +110,6 @@ function randomSpecialCharacter() {
   '/', ':', ';', '<', '=', '>','?', '@', '[', '\\', ']',  '^', '_', '`', '{', '|', '}', '~'];
   return specials[Math.floor(Math.random() * specials.length)];
 }
+
+// Add event listener to generate button. When the generate button is clicked, call writePassword function.
+generateBtn.addEventListener("click", writePassword);
